59bc56e9c96a1ae86fca879cdcb4186659dce75f
name is <my name>
2021-11-06 14:36:08 +0800

    上传串口通信，不定长收发，定时器和PWM的学习内容

---
+++ seris_comuni/MDK-ARM/RTE/_seris_comuni/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'seris_comuni' 
+ * Target:  'seris_comuni' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ seris_comuni/MDK-ARM/seris_comuni/startup_stm32f411xe.d
@@ -0,0 +1,1 @@
+seris_comuni\startup_stm32f411xe.o: startup_stm32f411xe.s
